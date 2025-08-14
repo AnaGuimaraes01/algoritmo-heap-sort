c2f164e5a9e4b3fa49d41367c411142ba61c769f
Ana_Carla_Guimarães <annacarlabarbosaguimaraes49@gmail.com>
2025-08-14 09:08:24 -0300

    Arquivo inicial heapsort.h

--- heapsort.h
+++ heapsort.h
@@ -1,1 +1,12 @@
   // Declaração das funções
+
+#ifndef HEAPSORT_H
+#define HEAPSORT_H
+
+// Função que realiza o Heap Sort no vetor
+void heapSort(int arr[], int n);
+
+// Função para imprimir um vetor
+void printArray(int arr[], int n);
+
+#endif
